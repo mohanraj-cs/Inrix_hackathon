42b37483c19fb6c546242f038b23bc4aa9ef7fe9
Anoop S <anoop.shivayogi@gmail.com>
2022-11-12 21:59:02 -0800

    Added real data for dummy GET method

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ from rtree import index
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
+global_co_ordinates = []
 
 def get_access_token():
     URL = 'https://api.iq.inrix.com/auth/v1/appToken?appId=yxjls34uih&hashToken=eXhqbHMzNHVpaHw1clJXZU1qNUoxNFFpUEVNVkRNemU3cU13dUtSZ01wcWE1dHRRTlJk'
     try:
-            response = requests.get(URL)
-            response_json = response.json()
-            token = response_json.get("result").get("token")
-            return token
+        response = requests.get(URL)
+        response_json = response.json()
+        token = response_json.get("result").get("token")
+        return token
     except Exception as e:
         print("Error while obtaining token", e)
         return ""
@@ -24,6 +24,7 @@ def get_access_token():
 # Args: source co-ordinate (top, left, bottom, right)
 # Return: An array: [id, [lat, long]]
 def nearest_point(source=(-122.4844171, 37.8587248, -122.4844171, 37.8587248), n=5):
+    global global_co_ordinates
     co_ordinates = []
     res = ''
     result = []
@@ -32,6 +33,8 @@ def nearest_point(source=(-122.4844171, 37.8587248, -122.4844171, 37.8587248), n
         res = json.load(r)["result"]
         for co_ordinate in res:
             co_ordinates.append(co_ordinate["geometry"]["coordinates"])
+        
+        global_co_ordinates = co_ordinates
 
         for i, item in enumerate(co_ordinates):
             idx.insert(i, (item[0], item[1], item[0], item[1]))
@@ -53,11 +56,15 @@ def hello():
 
 @app.route("/dummy")
 def dummy():
-    data = {"result": [
-                {"lat":1.0000, "long":2.000, "wait":1.0},
-                {"lat":2.0000, "long":3.000, "wait":1.5},
-                {"lat":4.0000, "long":5.000, "wait":15},
-    ]}
+    nearest_point()
+    global global_co_ordinates
+    data = ''
+    for i in global_co_ordinates:
+        data = {"result": [
+                    {"lat":i[0], "long":i[1], "wait":1.0},
+                    {"lat":i[0], "long":i[1], "wait":1.5},
+                    {"lat":i[0], "long":i[1], "wait":15},
+        ]}
     return json.dumps(data)
 
 if __name__ == '__main__':
