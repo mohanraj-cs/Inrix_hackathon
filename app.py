be3fa81ae914afbaf952bb4661889a6c0130790a
Anoop S <anoop.shivayogi@gmail.com>
2022-11-12 21:02:25 -0800

    Added dummy Get Method

--- app.py
+++ app.py
@@ -44,6 +44,15 @@ def hello():
     parse_json()
     return 'Hello, World!'
 
+@app.route("/dummy")
+def dummy():
+    data = {"result": [
+                {"lat":1.0000, "long":2.000, "wait":1.0},
+                {"lat":2.0000, "long":3.000, "wait":1.5},
+                {"lat":4.0000, "long":5.000, "wait":15},
+    ]}
+    return json.dumps(data)
+
 if __name__ == '__main__':
  app.run(debug=True)
 
